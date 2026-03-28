8d8db9a6766536d64c05ab45b16bbe9c6d57c577
Zelquent_Tech <zelquent.tech@gmail.com>
2026-03-28 13:22:36 +0530

    images chnaged

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,8 +1,13 @@
 import heroWedding from "@/assets/hero-wedding.jpg";
 import serviceWedding from "@/assets/service-wedding.jpg";
 import serviceBirthday from "@/assets/service-birthday.jpg";
+import serviceBirthday0 from "@/assets/service-birthday0.jpg";
+import serviceBirthday1 from "@/assets/service-birthday1.jpg";
 import serviceCatering from "@/assets/service-catering.jpg";
+import serviceCatering1 from "@/assets/service-catering1.jpg";
 import serviceCustom from "@/assets/service-custom.jpg";
+import serviceCustom1 from "@/assets/service-custom1.jpg";
+import wedding1 from "@/assets/wedding1.jpg";
 import gallery1 from "@/assets/gallery-1.jpg";
 import gallery2 from "@/assets/gallery-2.jpg";
 import gallery3 from "@/assets/gallery-3.jpg";
@@ -12,10 +17,15 @@ import gallery6 from "@/assets/gallery-6.jpg";
 
 export const images: Record<string, string> = {
   "hero-wedding": heroWedding,
+  "wedding1": wedding1,
   "service-wedding": serviceWedding,
   "service-birthday": serviceBirthday,
+  "service-birthday0": serviceBirthday0,
+  "service-birthday1": serviceBirthday1,
   "service-catering": serviceCatering,
+  "service-catering1": serviceCatering1,
   "service-custom": serviceCustom,
+  "service-custom1": serviceCustom1,
   "gallery-1": gallery1,
   "gallery-2": gallery2,
   "gallery-3": gallery3,
